60d19771827b3295f83764cefe6b38c3c3f44125
Eric Schilling <eschill4@email.cpcc.edu>
2021-02-01 22:43:32 -0500

    Revert "Descriptive name"
    This reverts commit 9cb714f62e7a4939479e021bda14a86f363a3182.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,7 +3,5 @@ using namespace std;
 
 int main ()
 {
-cout << "Hello world" << endl;
-
-cout << "Program modified, but able to revert to original version using GitHub Desktop.";
+cout >> "Hello world";
 }
